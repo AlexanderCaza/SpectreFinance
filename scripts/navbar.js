3a3613d8daccb16e50c19ac1b60f5fc0c0f7b548
AlexanderCaza <ajmc03@outlook.com>
2022-12-24 15:30:15 -0800

    Navbar deployed

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,1 +1,40 @@
-
+class Navbar extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <nav>
+            <ul>
+                <div class="dropdown">
+                    <button onclick="myFunction()" class="dropbtn">News Articles</button>
+                    <div id="myDropdown" class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <button onclick="myFunction()" class="dropbtn">Company Highlights</button>
+                    <div id="myDropdown" class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <button onclick="myFunction()" class="dropbtn">Monthly Stock Picks</button>
+                    <div id="myDropdown" class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                    </div>
+                </div>
+            </ul>
+        </nav>
+      `;
+    }
+  }
+  
+  customElements.define('nav-component', Navbar);
