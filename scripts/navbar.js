9c9a573904ef3f60d0d3d601184d7b7caeae1c5e
AlexanderCaza <ajmc03@outlook.com>
2022-12-25 14:05:04 -0800

    Responsive Design Added

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -10,25 +10,25 @@ class Navbar extends HTMLElement {
                 <div class="dropdown">
                     <button onclick="myFunction()" class="dropbtn">News Articles</button>
                     <div id="myDropdown" class="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
                     </div>
                 </div>
                 <div class="dropdown">
                     <button onclick="myFunction()" class="dropbtn">Company Highlights</button>
                     <div id="myDropdown" class="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
                     </div>
                 </div>
                 <div class="dropdown">
                     <button onclick="myFunction()" class="dropbtn">Monthly Stock Picks</button>
                     <div id="myDropdown" class="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
                     </div>
                 </div>
             </ul>
