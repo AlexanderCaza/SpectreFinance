b8beb8b7cfb72de66a164032836d40f243295e62
AlexanderCaza <ajmc03@outlook.com>
2023-02-05 13:58:57 -0800

    February 2023 Stock Picks

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -22,6 +22,7 @@ class Navbar extends HTMLElement {
                 <div class="dropdown">
                     <button onclick="myFunction()" class="dropbtn">Monthly Stock Picks</button>
                     <div id="myDropdown" class="dropdown-content">
+                        <a href="https://spectrefinance.org/pages/monthly-picks/february2023.html">February 2023</a>
                         <a href="https://spectrefinance.org/pages/monthly-picks/january2023.html">January 2023</a>
                     </div>
                 </div>
